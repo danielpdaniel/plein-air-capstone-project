94b38df74a54c33f1184b769f46b5244e430669a
danielpdaniel <artofjquinn@gmail.com>
2023-04-11 13:23:12 -0400

    set up User model has_secure_password + seed with sample user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+
+    def index
+        users = User.all
+        render json: users, status: :ok
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+    private
+
+    def user_params
+        params.permit(:username, :password, :about)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create({username: "hotdoggity", password: "cornchip123", about: "its me >:3"})
+
