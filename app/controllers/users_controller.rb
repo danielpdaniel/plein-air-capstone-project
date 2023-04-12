dd6420fccfb1ad9aa46cb80204bdd1254cc4d9fa
danielpdaniel <artofjquinn@gmail.com>
2023-04-12 11:48:42 -0400

    set up user controller index method to return one user or raise an exception if none found

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,11 @@ class UsersController < ApplicationController
         render json: users, status: :ok
     end
 
+    def show
+        user = User.find_by!(id: user_params[:id])
+        render json: user, status: :ok
+    end
+
     def create
         user = User.create!(user_params)
         render json: user, status: :created
