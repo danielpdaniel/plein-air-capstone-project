d07bfb0161d1dbd36179f5120944f143a9effe64
danielpdaniel <artofjquinn@gmail.com>
2023-04-13 14:34:54 -0400

    set pageUser in state to user context if params id matches user context id

--- client/src/UserProfile.js
+++ client/src/UserProfile.js
@@ -1,16 +1,22 @@
 import { useParams } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { useState, useEffect, useContext } from "react"
+import { UserContext } from "./context/user"
 
 function UserProfile(){
     const params = useParams()
     const [pageUser, setPageUser] = useState("")
+    const { user } = useContext(UserContext)
     useEffect(()=>{
+        if(params.id == user.id){
+            setPageUser(user)
+        }else{
         fetch(`/users/${params.id}`)
         .then(r=>{
             if(r.ok){
                 r.json().then(data => setPageUser(data))
             }
             })
+        }
     },[])
 
     return (
