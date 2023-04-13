0ff350ccaeb838b0f2e9b55c820d28a262d0d1e5
danielpdaniel <artofjquinn@gmail.com>
2023-04-13 13:07:23 -0400

    create authorize method in application controller + start setting up user context on frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::API
 include ActionController::Cookies
+before_action :authorize
 
 def hello_world
     session[:count] = (session[:count] || 0) + 1
     render json: { count: session[:count] }
+end
+
+def authorize
+    @user = User.find_by(id: session[:user_id])
 end
 end
---
+++ client/src/context/user.js
@@ -0,0 +1,5 @@
+import { useContext } from "react";
+
+const UserContext = React.createContext()
+
+export { UserContext, UserProvider}
