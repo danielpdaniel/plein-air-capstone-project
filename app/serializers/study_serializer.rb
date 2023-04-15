05e3f188e1342ad669c57eb085caf031d9d06b25
danielpdaniel <artofjquinn@gmail.com>
2023-04-15 19:18:18 -0400

    update study serializer to include study tags in JSON data

--- app/serializers/study_serializer.rb
+++ app/serializers/study_serializer.rb
@@ -1,8 +1,9 @@
 class StudySerializer < ActiveModel::Serializer
   attributes :id, :user_id, :location_id, :created_at, :attached_images, :images, :caption
 
-  def attached_images
+  has_many :tags
 
+  def attached_images
     object.study_images_urls
   end
 end
