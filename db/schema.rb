4f1637c30901e8c9aaa3a32cf92acf3453fa902f
danielpdaniel <artofjquinn@gmail.com>
2023-04-15 19:05:33 -0400

    create tag model and table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_15_160842) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_15_230422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -55,6 +55,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_15_160842) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "caption"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
