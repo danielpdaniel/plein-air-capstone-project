788ee0968e3213204a1db029e0086251f3541968
danielpdaniel <artofjquinn@gmail.com>
2023-04-15 12:42:49 -0400

    add edit and delete buttons to user profile study cards

--- client/src/StudyCard.js
+++ client/src/StudyCard.js
@@ -1,9 +1,28 @@
+import { useContext } from "react"
+import { UserContext } from "./context/user"
+
 function StudyCard({ study }){
-    console.log(study)
+    const {user} = useContext(UserContext)
+
+
+    function handleDeleteClick(e){
+        console.log(study.id)
+    }
 return (
     <div className="studyCard">
+        {study.user_id == user.id ? 
+        <div>
+            <button>
+                ✏️
+            </button>
+            <button onClick={handleDeleteClick}>
+                🗑️
+            </button>
+            
+        </div> : null}
         {study.attached_images.map(image =>  <img key={image} src={image}/>)}
         <p>{study.caption}</p>
+        <p>{study.id}</p>
     </div>
 )
 }
