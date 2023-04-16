6188d71e3e0c86602895f9a1197408699c6278f7
danielpdaniel <artofjquinn@gmail.com>
2023-04-15 20:50:02 -0400

    add additional form to new study component for adding tags to a study post

--- client/src/NewStudyForm.js
+++ client/src/NewStudyForm.js
@@ -5,6 +5,8 @@ import { useState } from "react"
 function NewStudyForm(){
     const [files, setFiles] = useState("")
     const [caption, setCaption] = useState("")
+    const [tags, setTags] = useState([])
+    const [currentTag, setCurrentTag] = useState("")
 
     function handleFileChange(e){
         const loadedFiles = []
@@ -38,14 +40,14 @@ function NewStudyForm(){
 
         formData.append("caption", caption)
        
-        for (const pair of formData.entries()) {
-            // if (pair[0] == "images"){
-            //     console.log(pair[0])
-            //     console.log(pair[1])
-            // }
-            console.log("foo")
+        // for (const pair of formData.entries()) {
+        //     // if (pair[0] == "images"){
+        //     //     console.log(pair[0])
+        //     //     console.log(pair[1])
+        //     // }
+        //     console.log("foo")
     
-          }
+        //   }
 
         // for (var pair of formData.entries()) {
         //     console.log(pair[0]+ ', ' + pair[1]); 
@@ -67,12 +69,23 @@ function NewStudyForm(){
             }
         })
     }
+
+    function handleAddTag(e){
+        e.preventDefault()
+        const updatedTags = tags.includes(currentTag) ? tags : [...tags, currentTag]
+        setTags(updatedTags)
+        setCurrentTag("")
+    }
 return(
     <div>
         <form onSubmit={(e)=>handleNewStudySubmit(e)}>
             <input type="file" accept="image/*" multiple={true} onChange={(e)=>setFiles(e.target.files)}/>
             <textarea value={caption} onChange={(e) =>setCaption(e.target.value)}/>
             <input type="submit"/>
+        </form>
+        {tags ? <p>{tags.map(tag => `#${tag}, `)}</p> : null}
+        <form onSubmit={(e) => handleAddTag(e)}>
+            <input type="text" value={currentTag} onChange={(e)=>setCurrentTag(e.target.value)} placeholder="add tags to your post!"/>
         </form>
     </div>
 )
