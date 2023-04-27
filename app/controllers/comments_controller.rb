8cbbb9442236124d2feff5999637eacec7f933ec
danielpdaniel <artofjquinn@gmail.com>
2023-04-27 16:43:20 -0400

    fix some bugs with comment POST route

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,13 +3,14 @@ class CommentsController < ApplicationController
 
 
     def create
-        comment = @user.studies.create!(comment_params)
+        # byebug
+        comment = @user.comments.create!(comment_params)
     end
 
     private
 
     def comment_params
-        params.permit(study_id, comment_text)
+        params.permit(:study_id, :comment_text)
     end
 
     def invalid_comment_response(invalid)
