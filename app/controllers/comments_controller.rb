d351e6429a50abcaaa162a566b293d5996443a70
danielpdaniel <artofjquinn@gmail.com>
2023-04-27 16:38:59 -0400

    set up new comment post route + controller handler

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+    rescue_from ActiveRecord::RecordInvalid, with: :invalid_comment_response
+
+
+    def create
+        comment = @user.studies.create!(comment_params)
+    end
+
+    private
+
+    def comment_params
+        params.permit(study_id, comment_text)
+    end
+
+    def invalid_comment_response(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+end
