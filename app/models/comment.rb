309aa8605e9ec5e5d7a1811ac9e6d9ee94a128c9
danielpdaniel <artofjquinn@gmail.com>
2023-04-27 16:39:58 -0400

    add validations to Comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :study
+
+    validates :user_id, presence: true
+    validates :study_id, presence: true
+    validates :comment_text, presence: true
 end
