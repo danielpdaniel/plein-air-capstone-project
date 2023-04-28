9de48ba3089a1ca79cb72745c6cfb03a38b101fe
danielpdaniel <artofjquinn@gmail.com>
2023-04-28 17:55:34 -0400

    Update individual study page with different sizings and their own classNames

--- client/src/Study.js
+++ client/src/Study.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import StudyCard from "./StudyCard"
+import CommentWindow from "./CommentWindow"
 
 function Study(){
     const params = useParams()
@@ -16,9 +17,9 @@ function Study(){
     }, [])
     
     return (
-        <div>
+        <div className="singleStudy">
             {study ? 
-            <StudyCard study={study} studyClassName="studyCard"/>
+            <StudyCard study={study} studyClassName="singleStudyCard"/>
             :
             <h5>Loading...</h5>}
         </div>
