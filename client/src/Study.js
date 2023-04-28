8a5b4ed915c68bd6164bce675159cccf906cccc6
danielpdaniel <artofjquinn@gmail.com>
2023-04-28 17:49:20 -0400

    Fetch study with useParams id then render studyCard component with that study

--- client/src/Study.js
+++ client/src/Study.js
@@ -1,17 +1,26 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import StudyCard from "./StudyCard"
 
 function Study(){
     const params = useParams()
-    const study = useState("")
+    const [study, setStudy] = useState("")
 
     useEffect(()=>{
-
+        fetch(`/studies/${params.id}`)
+        .then(r=>{
+            if(r.ok){
+                r.json().then(data=>setStudy(data))
+            }
+        })
     }, [])
     
     return (
         <div>
-            Study will go here
+            {study ? 
+            <StudyCard study={study} studyClassName="studyCard"/>
+            :
+            <h5>Loading...</h5>}
         </div>
     )
 }
