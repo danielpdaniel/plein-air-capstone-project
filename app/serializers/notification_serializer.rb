317c54317c0354b5005ed75eaf90e6ac3eacc322
danielpdaniel <artofjquinn@gmail.com>
2023-04-28 14:35:15 -0400

    create migration to add :author column to notifications table

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,4 +1,4 @@
 class NotificationSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :study_id, :read_status
-  byebug
+  attributes :id, :user_id, :study_id, :read_status, :study
+  
 end
---
+++ db/migrate/20230428183342_add_author_to_notifications.rb
@@ -0,0 +1,5 @@
+class AddAuthorToNotifications < ActiveRecord::Migration[7.0]
+  def change
+    add_column :notifications, :author, :integer
+  end
+end
