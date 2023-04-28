f6129c49f52f7f5c63e3e6a6ca79e757d84dbc0c
danielpdaniel <artofjquinn@gmail.com>
2023-04-28 14:20:02 -0400

    fetch notifications + update their read_status values to true + map returned values on frontend

--- client/src/Notifications.js
+++ client/src/Notifications.js
@@ -1,16 +1,26 @@
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import { UserContext } from "./context/user"
 
 function Notifications(){
 
     const { user } = useContext(UserContext)
+    const [notifications, setNotifications] = useState("")
+
+    useEffect(()=>{
+        fetch("/notifications")
+        .then(r=>{
+            if(r.ok){
+                r.json().then(data => setNotifications(data))
+            }
+        })
+    }, [])
 
     return(
         <div>
             {user ? 
             <div>
                 <h3>these are your notifications!</h3>
-                {user.notifications ? user.notifications.map(notif => <p>you got a notification for {notif.study_id}!</p>) : <p>no notifications yet :)</p>}
+                {notifications ? notifications.map(notif => <p>you got a notification for {notif.study_id}!</p>) : <p>no notifications yet :)</p>}
             </div>
             : <h2>Login/Signup to get started!</h2>}
         </div>
