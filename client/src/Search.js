1e397f3f227871276ec61bc6c4ad1dd062799f6e
danielpdaniel <artofjquinn@gmail.com>
2023-05-14 15:01:35 -0400

    Build out frontend Search component some more

--- client/src/Search.js
+++ client/src/Search.js
@@ -1,10 +1,38 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 function Search(){
     const [tagValue, setTagValue] = useState("")
+    const [tagEntry, setTagEntry] = useState("")
+    const [studies, setStudies] = useState("")
+    const [errors, setErrors] = useState("")
+
+    function handleSearchSubmit(e){
+        e.preventDefault()
+        setTagEntry(tagValue)
+    }
+
+    useEffect(()=>{
+        if(tagEntry){
+            fetch(`/tagged/${tagEntry}`)
+            .then(r=>{
+                if(r.ok){
+                    r.json().then(data=>setStudies(data))
+                }
+                else{
+                    r.json().then(data => setErrors(data.errors))
+                }
+            })
+        }
+    }, [tagEntry])
+
+    console.log(studies)
     return(
         <div>
-            <input type="text" value={tagValue} onChange={e => setTagValue(e.target.value)}/>
+            <h2>Search Studies By Tag</h2>
+            <form onSubmit={(e)=>handleSearchSubmit(e)}>
+                <input type="text" value={tagValue} onChange={e => setTagValue(e.target.value)} placeholder="search tag..."/>
+                <input type="submit"/>
+            </form>
         </div>
     )
 }
