eed6820efb2cced5b6568c332bf49a9e284eb64b
danielpdaniel <artofjquinn@gmail.com>
2023-05-14 15:30:40 -0400

    Order studies filtered by tags by created_at in DESC order

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-    has_many :studies_tags
+    has_many :studies_tags, -> {order(created_at: :DESC)}
     has_many :studies, through: :studies_tags
     has_many :locations, through: :studies
 
