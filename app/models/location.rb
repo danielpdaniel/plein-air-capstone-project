ff2d6f2cf10e1581513eb955557c56d2100bb8e7
danielpdaniel <artofjquinn@gmail.com>
2023-05-22 13:22:40 -0400

    Validates latitude and longitude presence since that's what's being used by the map markers

--- app/models/location.rb
+++ app/models/location.rb
@@ -2,8 +2,7 @@ class Location < ApplicationRecord
     belongs_to :study
     has_many :users, through: :studies
 
-    # validates :name, presence: true
-    # validates :longitude, presence: true
-    # validates :latitude, presence: true
+    validates :longitude, presence: true
+    validates :latitude, presence: true
     validates :lat_lng, presence: true
 end
